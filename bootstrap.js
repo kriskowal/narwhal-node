a3da30bc38e3cd05ecda903efc6a26bc0405a203
kriskowal <kris.kowal@cixar.com>
2010-02-15 15:26:42 -0800

    Tacking narwhal onto the side.

---
+++ bootstrap.js
@@ -0,0 +1,7 @@
+(function (modules) {
+    // This file is an anonymous function expression.  The file is executed in
+    // node_narwhal.cc to produce a Function.  The function is called in
+    // node_narwhal.cc with a module memo that has been "primed" with native
+    // modules to avoid communicating dependencies through the transitive
+    // globals.
+})
