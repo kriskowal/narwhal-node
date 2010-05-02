9acf435a6447744b7a74f96de66a60bc71cd62b8
kriskowal <kris.kowal@cixar.com>
2010-05-02 12:35:19 -0700

    Change of approach: getting this package working as an adapter for Narwhal based on unpatched Node.

--- bootstrap.js
+++ bootstrap.js
@@ -1,134 +1,116 @@
-(function (modules) {
 
-    // -- kriskowal Kris Kowal Copyright (C) 2009-2010 MIT License
+var modules = {};
+var PROCESS = process;
+var NODE_FS = require("fs");
+var SYS = require("sys");
+var FS, SYSTEM, ENGINE;
 
-    // This file is an anonymous function expression.  The file is executed in
-    // node_narwhal.cc to produce a Function.  The function is called in
-    // node_narwhal.cc with a module memo that has been "primed" with native
-    // modules to avoid communicating dependencies through the transitive
-    // globals.
+// -- kriskowal Kris Kowal Copyright (C) 2009-2010 MIT License
 
-    function main () {
-        try {
-            var text = FS.read(ENGINE.prefix + "/narwhal.js");
-            var factory = PROCESS.compile(text, "$NARWHAL/narwhal.js", 1);
-            factory(modules);
-        } catch (exception) {
-            if (exception instanceof Error) {
-                Error.captureStackTrace(this, main);
-                throw exception.stack;
-            } else {
-                throw exception;
+// This file is an anonymous function expression.  The file is executed in
+// node_narwhal.cc to produce a Function.  The function is called in
+// node_narwhal.cc with a module memo that has been "primed" with native
+// modules to avoid communicating dependencies through the transitive
+// globals.
+
+function main () {
+
+    var natives = PROCESS.binding("natives");
+    Object.keys(natives).forEach(function (name) {
+        Object.defineProperty(modules, name, {
+            "get": function () {
+                return require(name);
             }
-        }
-    }
+        });
+    });
 
-    // module "system"
-    (function (exports) {
+    var text = FS.read(ENGINE.prefix + "/narwhal.js");
+    var factory = PROCESS.compile(text, "$NARWHAL/narwhal.js", 1);
+    factory(modules);
+}
 
-        // exports.env = // node_narwhal.cc
+// module "system"
+(function (exports) {
 
-        exports.print = function () {
-            if (exports.stdout) {
-                return exports.stdout.print.apply(exports.stdout, arguments);
-            } else {
-                modules['process'].write(
-                    Array.prototype.join.call(arguments, " ") + "\n"
+    exports.env = PROCESS.env;
+    exports.args = [PROCESS.argv[0]].concat(PROCESS.argv.slice(2));
+    exports.print = SYS.puts;
+
+})(SYSTEM = modules['system'] = {});
+
+// module "narwhal/engine"
+(function (exports) {
+
+    exports.global = this;
+    exports.engine = "node";
+    exports.engines = ["node", "default"];
+    exports.debug = +SYSTEM.env["NARWHAL_DEBUG"];
+    exports.verbose = +SYSTEM.env["NARWHAL_VERBOSE"];
+    exports.strict = +SYSTEM.env["NARWHAL_STRICT"];
+
+    exports.os = undefined; // XXX TODO
+    exports.enginePrefix = SYSTEM.env["NARWHAL_ENGINE_HOME"];
+    exports.prefix = SYSTEM.env["NARWHAL_HOME"];
+    exports.prefixes = [exports.prefix];
+
+    ENGINE.compile = PROCESS.compile;
+
+    exports.Module = function (text, fileName, lineNo) {
+        var factory = function (inject) {
+            var names = [];
+            for (var name in inject)
+                if (Object.prototype.hasOwnProperty.call(inject, name))
+                    names.push(name);
+            var factory;
+            try {
+                factory = PROCESS.compile(
+                    Array(lineNo).join("\n") +
+                    "(function(" + names.join(",") + "){" + text + "\n})",
+                    fileName
+                );
+            } catch (exception) {
+                throw new Error(
+                    exception + " while compiling " +
+                    fileName + ":" + lineNo
                 );
             }
-            return exports;
+            factory.apply(null, names.map(function (name) {
+                return inject[name];
+            }));
         };
+        return factory;
+    };
 
-    }).call(this, modules.system);
-
-    // module "engine"
-    (function (exports, SYSTEM) {
-
-        exports.global = this;
-        exports.engine = "node";
-        exports.engines = ["node", "v8", "default"];
-        exports.debug = +SYSTEM.env["NARWHAL_DEBUG"];
-        exports.verbose = +SYSTEM.env["NARWHAL_VERBOSE"];
-
-        exports.os = undefined; // XXX TODO
-        exports.enginePrefix = SYSTEM.env["NARWHAL_ENGINE_HOME"];
-        exports.prefix = SYSTEM.env["NARWHAL_HOME"];
-        exports.prefixes = [exports.prefix];
-
-        exports.Module = function (text, fileName, lineNo) {
-            var factory = function (inject) {
-                var names = [];
-                for (var name in inject)
-                    if (Object.prototype.hasOwnProperty.call(inject, name))
-                        names.push(name);
-                var factory;
-                try {
-                    factory = exports.compile(
-                        Array(lineNo).join("\n") +
-                        "(function(" + names.join(",") + "){" + text + "\n})",
-                        fileName
-                    );
-                } catch (exception) {
-                    throw new Error(
-                        exception + " while compiling " +
-                        fileName + ":" + lineNo
-                    );
-                }
-                factory.apply(null, names.map(function (name) {
-                    return inject[name];
-                }));
-            };
-            return factory;
-        };
-
-    })(modules.engine, modules.system);
-
-    // module "narwhal/fs"
-    (function (exports) {
-        var K = modules['process'];
-        var NODE_FS = modules['process'].fs;
-
-        exports.isFile = function (path) {
-            try {
-                return (NODE_FS.stat(path).mode & K.S_IFREG) === K.S_IFREG;
-            } catch (e) {
-                return false;
+    ENGINE.loaders = [
+        [".node", {
+            "load": function (path) {
+                return function (inject) {
+                    var exports = require(path);
+                    for (var name in exports) {
+                        inject.exports[name] = exports[name];
+                    }
+                };
+            },
+            "reload": function (path) {
             }
-        };
+        }]
+    ];
 
-        exports.read = function (path) {
-            try {
-                var fd = NODE_FS.open(path, K.O_RDONLY, 0666);
-            } catch (exception) {
-                throw new Error("Failed to open " + path + ": " + exception);
-            }
-            try {
-                var chunks = [];
-                var pos = 0;
-                do {
-                    var chunkPair = NODE_FS.read(fd, 1024, pos, "utf8");
-                    var chunk = chunkPair[0];
-                    if (chunk === null)
-                        chunk = "";
-                    chunks.push(chunk);
-                    pos += chunkPair[1];
-                } while (chunk.length);
-                return chunks.join("");
-            } finally {
-                NODE_FS.close(fd);
-            }
-        };
+})(ENGINE = modules['narwhal/engine'] = {});
 
-    })(modules['narwhal/fs'] = {});
+// module "narwhal/fs"
+(function (exports) {
 
-    var PROCESS = modules['process'];
-    var FS = modules['narwhal/fs'];
-    var SYSTEM = modules['system'];
-    var ENGINE = modules['engine'];
+    exports.read = NODE_FS.readFileSync;
 
-    main();
+    exports.isFile = function (path) {
+        try {
+            var stat = NODE_FS.statSync(path);
+        } catch (x) {}
+        return stat && stat.isFile();
+    };
 
-    PROCESS.loop();
-    PROCESS.emit("exit");
+})(FS = modules['narwhal/fs'] = {});
 
-})
+main();
+
